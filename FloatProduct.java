15ebe79cde20dbf442980ac8d5019f210ce1b006
Kunal Nagar <160893021+CogitoKunal@users.noreply.github.com>
2024-10-06 15:53:07 +0530

    Add files via upload

---
+++ FloatProduct.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class FloatProduct {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter First Float Number:- ");
+        Float a=sc.nextFloat();
+        System.out.print("Enter Second Float Number:- ");
+        Float b=sc.nextFloat();
+        System.out.println("The Product of two Float Numbers is "+(a*b));
+
+
+    }
+}
---
+++ PerimeterRectange.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class PerimeterRectange {
+    public static void main(String[] args){
+        System.out.println("Welcome, to Perimeter Calculator..");
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter First side a ");
+        Float a=sc.nextFloat();
+        System.out.print("Enter First side a ");
+        Float b=sc.nextFloat();
+        System.out.print("Enter First side a ");
+        Float c=sc.nextFloat();
+        System.out.print("Enter First side a ");
+        Float d=sc.nextFloat();
+        System.out.println("Perimeter of Rectangle is "+(a+b+c+d));
+    }
+}
